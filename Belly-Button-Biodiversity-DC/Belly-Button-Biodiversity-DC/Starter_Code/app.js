1fb8f91b80f104fc3fa8111a29eebee19267fb65
DiegoCravis <68783192+DiegoCravis@users.noreply.github.com>
2020-11-18 15:30:07 -0600

    Add files via upload

---
+++ Belly-Button-Biodiversity-DC/Belly-Button-Biodiversity-DC/Starter_Code/app.js
@@ -0,0 +1,56 @@
+
+var drawChart = function(x_data, y_data, hoverText, metadata) {
+
+
+  var metadata_panel = d3.select("#sample-metadata");
+  metadata_panel.html("");
+  Object.entries(metadata).forEach(([key, value]) => {
+      metadata_panel.append("p").text(`${key}: ${value}`);
+  });
+
+  var trace = {
+      x: x_data,
+      y: y_data,
+      text: hoverText,
+      type: 'bar',
+      orientation: 'h'
+  };
+
+  var data = [trace];
+
+  Plotly.newPlot('bar', data);
+
+  var trace2 = {
+      x: x_data,
+      y: y_data,
+      text: hoverText,
+      mode: 'markers',
+      marker: {
+          size: y_data,
+          color: x_data
+      }
+  };
+
+  var data2 = [trace2];
+
+  Plotly.newPlot('bubble', data2);
+
+
+};
+
+var populateDropdown = function(names) {
+
+  var selectTag = d3.select("#selDataset");
+  var options = selectTag.selectAll('option').data(names);
+
+  options.enter()
+      .append('option')
+      .attr('value', function(d) {
+          return d;
+      })
+      .text(function(d) {
+          return d;
+      });
+
+};
+
